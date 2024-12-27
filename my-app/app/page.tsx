622574b077a8422b8fdf42443bab7832fb9d433e
David Shubov <116600844+drshooby@users.noreply.github.com>
2024-12-26 17:34:31 -0800

    pushing progress to dev

---
+++ my-app/app/page.tsx
@@ -0,0 +1,10 @@
+import NameHeader from "../components/NameHeader";
+import Background from "../components/Background";
+
+export default function Home() {
+    return (
+        <Background>
+            <NameHeader />
+        </Background>
+    )
+}
---
+++ my-app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+
+export default function Navbar() {
+    return (
+        <div>
+            <li>
+                <ul>
+                    LinkedIn
+                </ul>
+                <ul>
+                    Github
+                </ul>
+                <ul>
+                    Resume
+                </ul>
+            </li>
+        </div>
+    )
+}
