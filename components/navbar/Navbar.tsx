2b804459e8eda261d4ed626fbb9f176452a588c6
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-05-21 12:05:06 -0700

    start new nav

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,20 +1,52 @@
-import Link from "next/link"
+"use client";
+
 import styles from "./Navbar.module.css"
+import React, { useState } from 'react';
 
 export default function Navbar() {
+    const [currTab, setTab] = useState("About");
+
+    const renderContent = () => {
+        switch (currTab) {
+            case "About":
+                return <div>About me content goes here!</div>;
+            case "Portfolio":
+                return <div>My cool portfolio stuff goes here!</div>;
+            case "Contact":
+                return <div>Here's how to contact me!</div>;
+            default:
+                return <div>404 Tab not found</div>;
+        }
+    };
+
     return (
-        <div className={styles.navContainer}>
-            <ul className={styles.navList}>
-                <li className={styles.navItem}>
-                    <Link href={"https://www.linkedin.com/in/david-shubov/"}>LinkedIn</Link>
-                </li>
-                <li className={styles.navItem}>
-                    <Link href={"https://github.com/drshooby"}>GitHub</Link>
-                </li>
-                <li className={styles.navItem}>
-                    <Link href={"https://x.com/drshooby_"}>X/Twitter</Link>
-                </li>
-            </ul>
-        </div>
-    )
+        <>
+            <div className={styles.navContainer}>
+                <ul className={styles.navList}>
+                    <li 
+                        className={currTab === "About" ? styles.active : ""} 
+                        onClick={() => setTab("About")}
+                    >
+                        About
+                    </li>
+                    <li 
+                        className={currTab === "Portfolio" ? styles.active : ""} 
+                        onClick={() => setTab("Portfolio")}
+                    >
+                        Portfolio
+                    </li>
+                    <li 
+                        className={currTab === "Contact" ? styles.active : ""} 
+                        onClick={() => setTab("Contact")}
+                    >
+                        Contact
+                    </li>
+                </ul>
+            </div>
+
+            <div className={styles.contentArea}>
+                {renderContent()}
+            </div>
+        </>
+    );
 }
