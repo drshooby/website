593596710e7ba40701f45541ff86e0eb78791f83
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-05-31 19:50:57 -0700

    Update AboutText.ts

--- components/About/AboutText.ts
+++ components/About/AboutText.ts
@@ -1,5 +1,5 @@
 export const pageText = `
-   I enjoy backend systems and infrastructure. 
+   I enjoy backend systems and infrastructure.
    I build scalable services and automate workflows because consistency makes everything else a bit easier. 
    When I’m not coding, I capture life through photography — 
    it might not be front and center here, but it definitely inspires how I craft and style my code.
