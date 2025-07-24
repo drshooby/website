f7651c4b7eefd73c7c79b943a8fe378904666ffb
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-07-24 13:36:52 -0700

    update: project info

--- text/ProjectList.ts
+++ text/ProjectList.ts
@@ -7,7 +7,11 @@ export const projects = [
       "Provides flexible viewing and aggregation of transactions, customizable to each user’s needs.",
       "Currently focused on enhancing security features and optimizing database operations for reliability and performance."
     ],
-    techTags: ["TypeScript", "React.js", "Next.js", "Prisma", "PostgreSQL", "Plaid API", "Encryption"]
+    techTags: ["TypeScript", "React", "Next.js", "Prisma", "PostgreSQL", "Plaid API", "Encryption"],
+    awesomePeople: [
+      { name: "Edward", type: "personal", href: "https://edwardshturman.com/" },
+      { name: "Cal", type: "personal", href: "https://calum-crawford.com/" }
+    ]
   },
   {
     title: "Blink",
@@ -19,7 +23,12 @@ export const projects = [
       "Developed UI animations to enhance engagement and responsiveness.",
     ],
     techTags: ["Godot", "WASM", "GDScript", "Tweening"],
-    demo: { type: "video", src: "blink-demo.mp4" } as const
+    demo: { type: "video", src: "blink-demo.mp4" },
+    awesomePeople: [
+      { name: "Edward", type: "personal", href: "https://edwardshturman.com/" },
+      { name: "Cal", type: "personal", href: "https://calum-crawford.com/" },
+      { name: "Karl", type: "linkedin", href: "https://linkedin.com/in/karl-hu-josefsson-95998a300/"}
+    ]
   },
   {
     title: "Production-Grade Blue/Green Deployment Pipeline",
@@ -28,10 +37,10 @@ export const projects = [
       "Built and deployed scalable AWS infrastructure using Terraform, EKS, and GitHub Actions.",
       "Managed three FastAPI microservices across QA, UAT, and Prod environments.",
       "Configured RDS, ECR, TLS with Cert Manager, and secrets via Cluster Secret Store.",
-      "Automated blue/green deployments using Argo Rollouts and Shell scripting.",
+      "Automated blue/green deployments using Argo Rollouts and Bash scripting.",
     ],
-    techTags: ["Terraform", "AWS", "FastAPI", "GitHub Actions", "Kubernetes", "Helm", "Argo Rollouts", "Scripting"],
-    demo: { type: "image", src: "bg-repos-pic.png", alt: "blue/green deployment services picture"} as const
+    techTags: ["Terraform", "AWS", "FastAPI", "GitHub Actions", "Kubernetes", "Helm", "Argo Rollouts", "Bash"],
+    demo: { type: "image", src: "bg-repos-pic.png", alt: "blue/green deployment services picture"}
   },
   {
     title: "Blueprint",
@@ -40,8 +49,13 @@ export const projects = [
       "Won \"Most Innovative\" at DonsHack ‘25 and received a $500 micro-grant.",
       "Built a web tool to help USF students map degree progress and visualize co/prerequisites."
     ],
-    techTags: ["Python", "PostgreSQL", "React.js", "Next.js"],
-    demo: { type: "video", src: "blueprint-montage.mp4" } as const
+    techTags: ["Python", "PostgreSQL", "React", "Next.js"],
+    demo: { type: "video", src: "blueprint-montage.mp4" },
+    awesomePeople: [
+      { name: "Edward", type: "personal", href: "https://edwardshturman.com/" },
+      { name: "Cal", type: "personal", href: "https://calum-crawford.com/" },
+      { name: "Jet", type: "personal", href: "https://jetpham.com/"}
+    ]
   },
   {
     title: "Veil",
@@ -50,8 +64,12 @@ export const projects = [
       "Won 2nd Place at the USF Deploy/24 Hackathon and received a $500 micro-grant.",
       "Built an accessible full-stack app using object detection to locate and blur faces in videos for privacy and anonymity."
     ],
-    techTags: ["Python", "OpenCV", "Flask", "React.js", "Next.js"],
-    demo: { type: "video", src: "veil-demo.mp4" } as const
+    techTags: ["Python", "OpenCV", "Flask", "React", "Next.js", "Video Processing"],
+    demo: { type: "video", src: "veil-demo.mp4" },
+    awesomePeople: [
+      { name: "Andrew", type: "linkedin", href: "https://linkedin.com/in/andrew-diep-96b74a244/" },
+      { name: "Peter (demo)", type: "linkedin", href: "https://linkedin.com/in/peter-regalbuto/" }
+    ]
   }, 
   {
     title: "Ethical Web Crawler and Search Engine",
@@ -61,6 +79,6 @@ export const projects = [
       "Built a basic search engine using TF-IDF to rank and return relevant results from the indexed pages."
     ],
     techTags: ["Go", "HTML", "CSS", "SQLite"],
-    demo: { type: "image", src: "search-engine-pic.png", alt: "search engine picture"} as const
+    demo: { type: "image", src: "search-engine-pic.png", alt: "search engine picture"}
   }
 ];
