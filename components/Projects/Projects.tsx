1655c39456b4d9400e3ca783524b7e88fafd1bd6
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-07-24 13:38:17 -0700

    update: for refactoring

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -3,6 +3,9 @@ import styles from "./Projects.module.css";
 
 import { projects } from "@/text/ProjectList";
 
+import { DemoProps } from "@/types/demo";
+import { ContributorProps } from "@/types/contributor";
+
 export function Projects() {
   return (
     <div className={styles.container}>
@@ -16,7 +19,8 @@ export function Projects() {
             bullets={project.description}
             techTags={project.techTags}
             style={{ animationDelay: `${index * 0.1}s` }}
-            demo={project.demo}
+            demo={project.demo as DemoProps}
+            contributors={project.awesomePeople as ContributorProps[]}
           />
         ))}
       </div>
